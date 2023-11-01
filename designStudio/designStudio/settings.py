9dddaddfc2366c184d82597bad6baebd0822d7e1
Your Name <your_email@whatever.com>
2023-11-02 00:56:25 +0700

    Смена порта

--- designStudio/designStudio/settings.py
+++ designStudio/designStudio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.core.management.commands.runserver import Command as runserver
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+runserver.default.port = "8080"
+
