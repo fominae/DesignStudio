a09f5eee9e360e8d9815b35dcdaef5bf21917149
Your Name <your_email@whatever.com>
2023-11-02 01:04:26 +0700

    Добавление отображений

--- designStudio/Studio/views.py
+++ designStudio/Studio/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import RegisterUserForm
+from .models import Application
+
 
 # Create your views here.
+
+def index(request):
+    Application_title = Application.objects.all()
+    Application_description = Application.description
+    Application_category = Application.category
+    return render(request, 'index.html', context={'Application_title': Application_title,
+                                                 'Application_description': Application_description,
+                                                 'Application_category': Application_category})
+
+class RegisterView(CreateView):
+    template_name = 'registration/registration.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('login')
