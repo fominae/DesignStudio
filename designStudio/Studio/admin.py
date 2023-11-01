55c8b656fabe0049f0624172593c522e091b8c4a
Your Name <your_email@whatever.com>
2023-11-02 01:06:54 +0700

    Регистрация моделей и добавление отображения в админ-панели

--- designStudio/Studio/admin.py
+++ designStudio/Studio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Application,Category
 # Register your models here.
+
+admin.site.register(Category)
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'category')
