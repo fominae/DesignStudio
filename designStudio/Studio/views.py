838e96662eb8384a8acda259b63742e23bc2d9d2
Your Name <your_email@whatever.com>
2023-11-02 16:27:26 +0700

    Создание отображения заявок на главной страницы

--- designStudio/Studio/views.py
+++ designStudio/Studio/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .forms import RegisterUserForm
 from .models import Application
 
 
 # Create your views here.
 
-def index(request):
-    Application_title = Application.objects.all()
-    Application_description = Application.description
-    Application_category = Application.category
-    return render(request, 'index.html', context={'Application_title': Application_title,
-                                                  'Application_description': Application_description,
-                                                  'Application_category': Application_category})
 
 
 class RegisterView(CreateView):
     template_name = 'registration/register.html'
     form_class = RegisterUserForm
     success_url = reverse_lazy('login')
+
+
+class ViewRequests(ListView):
+   model = Application
+   template_name = 'index.html'
+   context_object_name = 'applications'
